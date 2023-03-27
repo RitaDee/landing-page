e6bdde2d33d0874fd9ed35d26f5d4c5565788ca3
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-27 16:50:28 +0100

    Update Banner.js file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Media } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import GirlImg from '../Images/Girl.png';
 
 const Banner = () => (
-  <Media className="align-items-center">
-    <Media.Body>
-      <h1>Grow your skills to advance your career path </h1>
-      <p>
-        build your future with our quality education.
-        the best and largest all-in-one online tutoring platform in the world
-      </p>
-    </Media.Body>
-    <img
-      width={64}
-      height={64}
-      className="ms-3"
-      src="../Images/Girl.png"
-      alt="placeholder"
-    />
-  </Media>
+  <Container>
+    <Row className="px-md-5">
+      <Col md={6}>
+        <h2>Grow your skills to advance your career path</h2>
+        <p>
+          Build your future with our quality education.
+          We are the best and largest all-in-one online tutoring platform in the world.
+        </p>
+        <Button variant="secondary" size="sm" className="mr-2">
+          Get started now
+        </Button>
+        <Button variant="primary" size="sm">
+          Enroll now
+        </Button>
+      </Col>
+      <Col md={6} className="text-center">
+        <img alt="girl" src={GirlImg} />
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Banner;
