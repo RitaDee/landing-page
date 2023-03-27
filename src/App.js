b9b5425de4b582be40c7ca284a9ad2a11056ff41
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-27 15:50:42 +0100

    Modify Navbar.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CustomNavbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function CustomNavbar() {
   return (
     <>
-      {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">techtime</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+      <Navbar bg="light" expand="sm" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Tech Time</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Collapse id="offcanvasNavbar">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">About us</Nav.Link>
+              <Nav.Link href="#action2">Courses</Nav.Link>
+              <Nav.Link href="#action2">Testimonial</Nav.Link>
+              <Nav.Link href="#action2">Community</Nav.Link>
+              <Nav.Link href="#action2">Enroll Now</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
