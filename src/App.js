f93b87af9ec3022c72c56e6ae167d2c381de1c3b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-27 14:53:54 +0100

    Add npm install react react-dom

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CustomNavbar from './components/Navbar';
+
+const App = () => (
+  <div>
+    <CustomNavbar />
+  </div>
+);
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,38 @@
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-const CustomNavbar = () => (
-  <Navbar bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <li>techtime</li>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#about">about us</Nav.Link>
-        <Nav.Link href="#courses">courses</Nav.Link>
-        <Nav.Link href="#testimonial">testimonial</Nav.Link>
-        <Nav.Link href="#community">community</Nav.Link>
-        <Nav.Link href="#enroll">enroll now</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
-);
+function CustomNavbar() {
+  return (
+    <>
+      {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">techtime</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
 
 export default CustomNavbar;
