8a403aef9239753f8c16730e039c6811bd9eb722
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-28 13:23:14 +0100

    Add more cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Audio from '../Images/volume_up.png';
 import antenna from '../Images/antenna.png';
@@ -13,6 +12,10 @@ import Techg from '../Images/techgirl.png';
 import User from '../Images/user.png';
 import Unsplash from '../Images/unsplash.png';
 import Education from '../Images/education.png';
+import Schedule from '../Images/schedule.png';
+import Group from '../Images/groups.png';
+import Atm from '../Images/atm.png';
+import Research from '../Images/research.png';
 import '../styles/About.css';
 
 const About = () => (
@@ -79,17 +82,28 @@ const About = () => (
         <Col md={6}>
           <Card>
             <Card.Body>
-                <Card.Img variant="top" src={Education} alt="Image" />
-              <Card.Title>Card 1</Card.Title>
-              <Card.Text>Some text for Card 1.</Card.Text>
+              <Card.Img variant="primary" src={Education} alt="Image" />
+              <Card.Title>Experienced Mentors</Card.Title>
+              <Card.Text>
+                High-Defination Video is Video of Higher Resolution And
+                Quality Than Standard Definition.
+                While There’s No Standard Meaning For High Definition,
+                Generally Any Standard Video Image
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={6}>
           <Card>
             <Card.Body>
-              <Card.Title>Card 2</Card.Title>
-              <Card.Text>Some text for Card 2.</Card.Text>
+              <Card.Img variant="primary" src={Schedule} alt="Image" />
+              <Card.Title>One-On-One Meetings</Card.Title>
+              <Card.Text>
+                High-Defination Video is Video of Higher Resolution And
+                Quality Than Standard Definition.
+                While There’s No Standard Meaning For High Definition,
+                Generally Any Standard Video Image
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
@@ -98,21 +112,114 @@ const About = () => (
         <Col md={6}>
           <Card>
             <Card.Body>
-              <Card.Title>Card 3</Card.Title>
-              <Card.Text>Some text for Card 3.</Card.Text>
+              <Card.Img src={Group} alt="Image" />
+              <Card.Title>One-On-One Meetings</Card.Title>
+              <Card.Text>
+                High-Defination Video is Video of Higher Resolution And
+                Quality Than Standard Definition.
+                While There’s No Standard Meaning For High Definition,
+                Generally Any Standard Video Image
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={6}>
           <Card>
             <Card.Body>
-              <Card.Title>Card 4</Card.Title>
-              <Card.Text>Some text for Card 4.</Card.Text>
+              <Card.Img src={Atm} alt="Image" />
+              <Card.Title>Affordable Prices</Card.Title>
+              <Card.Text>
+                High-Defination Video is Video of Higher Resolution And
+                Quality Than Standard Definition.
+                While There’s No Standard Meaning For High Definition,
+                Generally Any Standard Video Image
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
       </Row>
     </Container>
+    <div>
+      <h1>Browse Our Popular Courses</h1>
+      <Card>
+        <Card.Img variant="top" src={Research} />
+        <Card.Body>
+          <Card.Text>
+            Design
+            <span className="text-muted">Likes: 10</span>
+          </Card.Text>
+          <Card.Title>Introduction to user research in UX Design</Card.Title>
+          <Card.Text>
+            23hrs 50mins | 15 Lessons
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>Card Title 2</Card.Title>
+          <Card.Text>
+            <span className="text-muted">Likes: 20</span>
+          </Card.Text>
+          <Card.Text>Main Title 2</Card.Text>
+          <Card.Text>
+            Number of lessons: 10 | Hours: 5
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>Card Title 3</Card.Title>
+          <Card.Text>
+            <span className="text-muted">Likes: 30</span>
+          </Card.Text>
+          <Card.Text>Main Title 3</Card.Text>
+          <Card.Text>
+            Number of lessons: 15 | Hours: 8
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>Card Title 4</Card.Title>
+          <Card.Text>
+            <span className="text-muted">Likes: 40</span>
+          </Card.Text>
+          <Card.Text>Main Title 4</Card.Text>
+          <Card.Text>
+            Number of lessons: 20 | Hours: 10
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>Card Title 5</Card.Title>
+          <Card.Text>
+            <span className="text-muted">Likes: 50</span>
+          </Card.Text>
+          <Card.Text>Main Title 5</Card.Text>
+          <Card.Text>
+            Number of lessons: 25 | Hours: 12
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+          <Card.Title>Card Title 6</Card.Title>
+          <Card.Text>
+            <span className="text-muted">Likes: 60</span>
+          </Card.Text>
+          <Card.Text>Main Title 6</Card.Text>
+          <Card.Text>
+            Number of lessons: 30 | Hours: 15
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   </div>
 );
 
