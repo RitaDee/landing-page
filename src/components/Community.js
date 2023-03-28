4d59e443aa59ee2cd82445938368934c22ab0274
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-28 14:35:36 +0100

    Update Community.js file

---
+++ src/components/Community.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Comm from '../Images/community.png';
+
+const Community = () => (
+  <>
+    <div>
+      <h2>community.png</h2>
+      <h1>Are you  ready to connect with the future talent of the tech world</h1>
+      <p>Meet Up With Other Techstars And Grow Together</p>
+      <img alt="img" src={Comm} />
+    </div>
+
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <p>
+        High-Defination Video is Video of Higher Resolution And
+        Quality Than Standard Definition.
+        While There’s No Standard Meaning For High Definition,
+        Generally Any Standard Video Image
+      </p>
+    </div>
+    <div>
+      <p>Is there a free trial available?</p>
+      <p>
+        High-Defination Video is Video of Higher Resolution And
+        Quality Than Standard Definition.
+        While There’s No Standard Meaning For High Definition,
+        Generally Any Standard Video Image
+      </p>
+      <div className="container">
+        <div className="row">
+          <div className="col d-flex align-items-center">
+            <i className="fas fa-plus mr-2" />
+            Row 1
+          </div>
+        </div>
+        <div className="row">
+          <div className="col d-flex align-items-center">
+            <i className="fas fa-plus mr-2" />
+            Row 2
+          </div>
+        </div>
+        <div className="row">
+          <div className="col d-flex align-items-center">
+            <i className="fas fa-plus mr-2" />
+            Row 3
+          </div>
+        </div>
+        <div className="row">
+          <div className="col d-flex align-items-center">
+            <i className="fas fa-plus mr-2" />
+            Row 4
+          </div>
+        </div>
+        <div className="row">
+          <div className="col d-flex align-items-center">
+            <i className="fas fa-plus mr-2" />
+            Row 5
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </>
+);
+
+export default Community;
