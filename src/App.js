9846c286ba828170546a17fc62b8bbbc1088e7e1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-28 09:05:56 +0100

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import CustomNavbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Banner';
+import Client from './components/Client';
 
 const App = () => (
   <div>
     <CustomNavbar />
     <Banner />
+    <Client />
   </div>
 );
 
