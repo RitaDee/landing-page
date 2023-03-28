1b47b83e9ba71cb36201514c5e5948b0811f5557
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-28 14:01:51 +0100

    Update About.js file

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 import { Container, Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Audio from '../Images/volume_up.png';
@@ -16,6 +17,11 @@ import Schedule from '../Images/schedule.png';
 import Group from '../Images/groups.png';
 import Atm from '../Images/atm.png';
 import Research from '../Images/research.png';
+import Audience from '../Images/audience.png';
+import Code from '../Images/code.png';
+import Js from '../Images/js.png';
+import Room from '../Images/outroom.png';
+import Live from '../Images/live.png';
 import '../styles/About.css';
 
 const About = () => (
@@ -141,6 +147,12 @@ const About = () => (
     </Container>
     <div>
       <h1>Browse Our Popular Courses</h1>
+      <Nav className="justify-content-center flex-grow-1 pe-3">
+        <Nav.Link href="#action1">All Categories </Nav.Link>
+        <Nav.Link href="#action2">Design</Nav.Link>
+        <Nav.Link href="#action2">Development</Nav.Link>
+        <Nav.Link href="#action2">marketing</Nav.Link>
+      </Nav>
       <Card>
         <Card.Img variant="top" src={Research} />
         <Card.Body>
@@ -155,70 +167,73 @@ const About = () => (
         </Card.Body>
       </Card>
       <Card>
-        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Img variant="top" src={Audience} />
         <Card.Body>
-          <Card.Title>Card Title 2</Card.Title>
           <Card.Text>
+            marketing
             <span className="text-muted">Likes: 20</span>
           </Card.Text>
-          <Card.Text>Main Title 2</Card.Text>
+          <Card.Title>Introduction to  new marketing audience </Card.Title>
           <Card.Text>
-            Number of lessons: 10 | Hours: 5
+            22hrs 30mins | 22 Lessons
           </Card.Text>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Img variant="top" src={Code} />
         <Card.Body>
-          <Card.Title>Card Title 3</Card.Title>
           <Card.Text>
+            development
             <span className="text-muted">Likes: 30</span>
           </Card.Text>
-          <Card.Text>Main Title 3</Card.Text>
+          <Card.Title>Introduction to HTML, CSS & Bootstrap</Card.Title>
           <Card.Text>
-            Number of lessons: 15 | Hours: 8
+            45hrs 50mins | 55 lessons
           </Card.Text>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Img variant="top" src={Js} />
         <Card.Body>
-          <Card.Title>Card Title 4</Card.Title>
+          Development
           <Card.Text>
             <span className="text-muted">Likes: 40</span>
           </Card.Text>
-          <Card.Text>Main Title 4</Card.Text>
+          <Card.Title>Introduction to javascript, Git & GitHub</Card.Title>
           <Card.Text>
-            Number of lessons: 20 | Hours: 10
+            30hrs 50mins | 22 lessons
           </Card.Text>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Img variant="top" src={Room} />
         <Card.Body>
-          <Card.Title>Card Title 5</Card.Title>
           <Card.Text>
+            Marketing
             <span className="text-muted">Likes: 50</span>
           </Card.Text>
-          <Card.Text>Main Title 5</Card.Text>
+          <Card.Title>Introduction to outroom  marketing analysis</Card.Title>
           <Card.Text>
-            Number of lessons: 25 | Hours: 12
+            33hrs 50mins | 26 Lessons
           </Card.Text>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Img variant="top" src={Live} />
         <Card.Body>
-          <Card.Title>Card Title 6</Card.Title>
           <Card.Text>
+            Marketing
             <span className="text-muted">Likes: 60</span>
           </Card.Text>
-          <Card.Text>Main Title 6</Card.Text>
+          <Card.Title>Introduction to live  marketing analysis</Card.Title>
           <Card.Text>
-            Number of lessons: 30 | Hours: 15
+            10hrs 50mins | 32 lessons
           </Card.Text>
         </Card.Body>
       </Card>
+      <Button variant="primary" size="sm">
+        Explore All Courses
+      </Button>
     </div>
   </div>
 );
