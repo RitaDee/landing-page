f353c3072780c8b1584a82e601ca0588f4bdd9d8
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-28 09:37:01 +0100

    Add About.css file

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Card, Row, Col, Button,
+} from 'react-bootstrap/Card';
+
+const About = () => (
+  <div>
+    <Row className="px-md-5 mt-3">
+      {' '}
+      <Col md={6}>
+        <h2>High quality video, audio & live classes</h2>
+        <p>
+          High-Defination Video is Video of Higher Resolution And Quality Than Standard Definition.
+          While There’s No Standard Meaning For High Definition, Generally Any Standard Video Image
+        </p>
+        <Button variant="secondary" size="sm" className="mr-2">
+          View Courses
+        </Button>
+        <Button variant="primary" size="sm">
+          Enroll now
+        </Button>
+      </Col>
+      <Col md={6} className="text-center">
+        <img alt="girl" src={GirlImg} />
+      </Col>
+    </Row>
+
+  </div>
+);
+
+export default About;
