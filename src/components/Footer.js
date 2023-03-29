9c8bb2202f28de48683b5c328ff53b185e01f8cb
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-29 15:46:44 +0100

    Update Navbar.js file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
-import icon1 from '../Images/icons/g12.png'
-import icon2 from '../Images/icons/Twitter.png'
-import icon3 from '../Images/icons/YouTube.png'
-import icon4 from '../Images/icons/inst.png'
-import icon5 from '../Images/icons/Discord.png'
+import icon1 from '../Images/icons/g12.png';
+import icon2 from '../Images/icons/Twitter.png';
+import icon3 from '../Images/icons/YouTube.png';
+import icon4 from '../Images/icons/inst.png';
+import icon5 from '../Images/icons/Discord.png';
+
 function Footer() {
   return (
     <Container fluid id="footer">
@@ -18,12 +19,12 @@ function Footer() {
           <h3 className="logo">Tech Time</h3>
           <p>reach out to us on any of our social media networks</p>
           <div className="social-icons">
-              <img src={icon1} alt="" />
-              <img src={icon2} alt="" />
-              <img src={icon3} alt="" />
-              <img src={icon4} alt="" />
-              <img src={icon5} alt="" />
-          </div>  
+            <img src={icon1} alt="img" />
+            <img src={icon2} alt="img" />
+            <img src={icon3} alt="img" />
+            <img src={icon4} alt="img" />
+            <img src={icon5} alt="img" />
+          </div>
         </Col>
         <Col md={3}>
           <h4>Useful Links</h4>
@@ -60,6 +61,6 @@ function Footer() {
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
 export default Footer;
